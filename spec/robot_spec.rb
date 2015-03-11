703c680c2cf87f5d013398b76c17ca955045dec1
Kira Cheung <killercentury@gmail.com>
2015-03-11 22:47:30 +1100

    add a simple unit test case for report method

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,10 +2,17 @@ require 'robot'
 
 RSpec.describe Robot do
   describe "#set" do
-    it "robot is set to (0,1) and face north" do
+    it "is set to a position and orientation" do
       robot = Robot.new
       robot.set(0, 1, 'NORTH')
       expect(robot.report).to eq('0,1,NORTH')
+    end
+  end
+  describe "#report" do
+    it "reports its current position and orientation" do
+      robot = Robot.new
+      robot.set(2, 3, 'EAST')
+      expect(robot.report).to eq('2,3,EAST')
     end
   end
 end
